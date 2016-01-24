f9570807bafb3e65491e1b21e623cdc11ecdb85f
rakesh <rakesh.r1303@gmail.com>
2016-01-25 02:05:29 +0530

    Update

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -15,9 +15,18 @@ public class GreetingController {
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
     	count++;
     	System.out.println(count);
-        return new Greeting(counter.incrementAndGet(),
+        
+    	return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
+    
+    @RequestMapping("/index")
+    public String home(@RequestParam(value="home", defaultValue="World") String name) {
+    	count++;
+    	System.out.println(count);
+    	return "<!DOCTYPE html><html><script src='http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js'></script><body><div ng-app='myApp' ng-controller='customersCtrl'>Hi <a href='#' ng-click='call()'>Get</a><input type='text' ng-keyup='call()'> <div> {{con}}</div></div><script>var app = angular.module('myApp', []);app.controller('customersCtrl', function($scope, $http) {$scope.con=''; $scope.call= function (){$http.get('http://localhost:8080/greeting').then(function (response) {$scope.con= $scope.con + response.data.content;});};});</script></body></html>";
+    }
+    
     
     @RequestMapping("greeting/login")
     public Greeting login(@RequestParam(value="password", defaultValue="*****") String password, @RequestParam(value="id", defaultValue="Guest") String id) {
